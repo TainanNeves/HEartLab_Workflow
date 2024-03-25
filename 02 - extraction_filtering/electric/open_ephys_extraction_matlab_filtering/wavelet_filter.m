b8a00fd0a0a0d131f5a16dae4478c50a959ad48c
angelicadri <adsq04@hotmail.com>
2024-03-24 21:01:33 -0300

    plot and returned variable problem fixed

--- 02 - extraction_filtering/electric/open_ephys_extraction_matlab_filtering/wavelet_filter.m
+++ 02 - extraction_filtering/electric/open_ephys_extraction_matlab_filtering/wavelet_filter.m
@@ -49,83 +49,6 @@
 %
 %
 function data_filtered = wavelet_filter(D_EL, fs, electrodes, waveletTypes, numLevels, reconstructionLevelsSets, saveMRA)
-    data_filtered = [];
-    % check if saveMRA is provided, if not, set to false
-    if nargin < 7
-        saveMRA = false;
-    end
-    
-    % Iterate over each set of reconstruction levels
-    for i = 1:numel(reconstructionLevelsSets)
-        currentLevels = reconstructionLevelsSets{i};
-        
-        % Get the string representation of the current reconstruction levels
-        levelsString = strjoin(arrayfun(@num2str, currentLevels, 'UniformOutput', false), '_');
-        
-        % Iterate over each wavelet type
-        for j = 1:numel(waveletTypes)
-            currentWavelet = waveletTypes{j};
-            
-            % Initialize variables to store signals and MRA matrices for the current set and wavelet type
-            signals_currentLevelsWavelet = [];
-            mra_currentLevelsWavelet = [];
-
-            % Iterate over each electrode value
-            for k = 1:numel(electrodes)
-                electrode = electrodes(k);
-                
-                % Extract the electrode number from the loop index
-                electrodeName = ['Electrode_' num2str(electrode)];
-
-                % Original signal
-                Signal = D_EL(electrode, :);
-
-                % Applying conversion factor
-                factor_conv = 0.1949999928474426;
-                Signal = Signal * factor_conv;
-
-                % Decomposition using modwt
-                wt = modwt(Signal, currentWavelet, numLevels);
-
-                % Construct MRA matrix using modwtmra
-                mra_temp = modwtmra(wt, currentWavelet);
-
-                % Concatenate MRA matrix with previous ones
-                if isempty(mra_currentLevelsWavelet)
-                    mra_currentLevelsWavelet = mra_temp;
-                else
-                    mra_currentLevelsWavelet = cat(1, mra_currentLevelsWavelet, mra_temp);
-                end
-                            
-                % Assign MRA matrix to a variable named according to the electrode if saveMRA is true
-                if saveMRA
-                    variable_name_mra = ['MRA_' currentWavelet '_Levels_' levelsString '_Electrode_' num2str(electrode)];
-                    assignin('caller', variable_name_mra, mra_temp);
-                end
-
-                % Concatenate signals from all electrodes
-                if isempty(signals_currentLevelsWavelet)
-                    signals_currentLevelsWavelet = Signal;
-                else
-                    signals_currentLevelsWavelet = cat(1, signals_currentLevelsWavelet, Signal);
-                end
-            end
-
-            % Logical array for selecting reconstruction levels
-            currentRange = currentLevels;
-            levelForReconstruction = false(1, numLevels);
-            levelForReconstruction(currentRange(1):currentRange(2)) = true;
-
-            % Signal reconstruction by summing the selected rows
-            Signal_reconstructed = sum(mra_currentLevelsWavelet(levelForReconstruction, :), 1);
-            data_filtered = cat(1, data_filtered,Signal_reconstructed);
-
-            % Save the reconstructed signal for the current levels and wavelet type
-            variable_name_signals = ['Signal_reconstructed_' currentWavelet '_Levels_' levelsString];
-            assignin('caller', variable_name_signals, Signal_reconstructed);
-        end
-    end
-endfunction data_filtered = wavelet_filter(D_EL, fs, electrodes, waveletTypes, numLevels, reconstructionLevelsSets, saveMRA)
     
     % check if saveMRA is provided, if not, set to false
     if nargin < 7
@@ -154,7 +77,7 @@ endfunction data_filtered = wavelet_filter(D_EL, fs, electrodes, waveletTypes, n
                 electrode = electrodes(k);
                 
                 % Original signal
-                Signal = D_EL.Data(electrode, :);
+                Signal = D_EL(electrode, :);
 
                 % Applying conversion factor to microvolts
                 factor_conv = 0.1949999928474426;
