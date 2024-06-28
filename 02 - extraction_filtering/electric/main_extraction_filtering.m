8a984a79c53640960cb5604e6fe4cfacfa162069
TainanNeves <tainancneves@gmail.com>
2024-06-28 11:03:28 -0300

    Update

--- 02 - extraction_filtering/electric/main_extraction_filtering.m
+++ 02 - extraction_filtering/electric/main_extraction_filtering.m
@@ -41,12 +41,16 @@ mean_tank = mean(tank_signals);
 DATA.Data([129:174,177:190], :) = tank_signals - mean_tank;
 
 
-%% Add 60 Hz notch filter
+%% Extra notch filter
+% Run it just if you want to apply an extra layer of filtering
+% This is a temporal code. Final version must have this filtering inside
+% the filter_signal function.
 Fs = DATA.Header.sample_rate; % Sampling frequency
 notch_freq = 60; % Frequency to be removed (60Hz)
 bandwidth = 2; % Bandwidth of the notch filter
 [b, a] = iirnotch(notch_freq/(Fs/2), bandwidth/(Fs/2)); % Design 60Hz notch filter
 DATA.Data = filtfilt(b, a, DATA.Data); % Apply 60Hz notch filter to data, DATA.Data); % Apply 60Hz notch filter to data
+% Not conclusive if there is an increasement of filtering quality.
 
 
 %% Comparation Plot
