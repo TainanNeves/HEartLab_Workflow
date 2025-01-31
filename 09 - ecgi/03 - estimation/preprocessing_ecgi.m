d5b5621e5adba6d2958c1930fa30a6ac000061d1
angelicadri <adsq04@hotmail.com>
2025-01-31 12:59:18 -0300

    correção de erro no cálculo do espectro

--- 09 - ecgi/03 - estimation/preprocessing_ecgi.m
+++ 09 - ecgi/03 - estimation/preprocessing_ecgi.m
@@ -35,27 +35,34 @@
 % The script processes both measured signals from the MEAs and tank torso separately.  
 % It includes visualization steps to evaluate the impact of each preprocessing technique on the data.
 
+%% Loading the raw signal
+
+file_name = "C:\Users\HeartLAB\Documents\Documents\Codes\Current codes\02 - extraction_filtering\electric\open_ephys_extraction_matlab_filtering\electric_data_E20_F01_R01_raw.mat";
+data = load(file_name);
+meas_signal_raw = data.D_EL.Data([1:32, 65:80], :);
+tank_signal_raw = data.D_EL.Data([129:174, 177:190], :);
+
 
 %% Low-Pass Filtering
 % Define cutoff frequency
+fs = 4000;
 f_cut = 70 / (fs / 2);
 [b, a] = butter(6, f_cut, 'low');
-
 % Apply zero-phase filtering
-meas_signal_f = filtfilt(b, a, meas_signal')';
-tank_signal_f = filtfilt(b, a, tank_signal')';
+meas_signal_f = filtfilt(b, a, meas_signal_raw')';
+tank_signal_f = filtfilt(b, a, tank_signal_raw')';
 
 %% Define Time and Frequency Parameters
 in_time = 1;
 end_time = 3;
-in_sample = in_time * Fs + 1;
-end_sample = end_time * Fs;
+in_sample = in_time * fs + 1;
+end_sample = end_time * fs;
 time = linspace(in_time, end_time, length(in_sample:end_sample));
 
 f_up = 60;
 f_down = 0.5;
-in_sample = 1*Fs+1;
-end_sample = 3*Fs;
+in_sample = 1*fs+1;
+end_sample = 3*fs;
 
 %% Plot MEAs spectrum and electrical signals
 
@@ -79,7 +86,7 @@ for mea_idx = 1:3
     end
 
     mea_temp = meas_signal_f(mea_range, in_sample:end_sample); % Extract the signals for current MEA
-    [MFFTi, Sffti, fstep] = f_DF_electric(mea_temp, Fs, f_up, f_down);
+    [MFFTi, Sffti, fstep] = f_DF_electric(mea_temp, fs, f_up, f_down);
 
     % Loop through each set of 2 electrodes per plot (adjusted for indexing)
     for i = 1:2:16 % Grouping 2 electrodes per plot
@@ -99,7 +106,7 @@ for mea_idx = 1:3
 
             % Plot Electrical Signal for the current electrode 
             subplot(2, 2, j*2 + 2); 
-            plot(time, meas_signal(mea_range(electrode_idx), in_sample:end_sample), 'r', 'LineWidth', 1);
+            plot(time, meas_signal_raw(mea_range(electrode_idx), in_sample:end_sample), 'r', 'LineWidth', 1);
             hold on;
             plot(time, meas_signal_f(mea_range(electrode_idx), in_sample:end_sample), 'k', 'LineWidth', 1);
             title(['Electrical Signal - Electrode ' num2str(plot_range(electrode_idx)) ' - Cutoff: ' num2str(f_cut * fs / 2) ' Hz']);
@@ -123,7 +130,7 @@ tank_range = 1:60;
 tank_labels = [129:174, 177:190]; % Labels for plots
 
 tank_temp = tank_signal_f(tank_range, in_sample:end_sample); % Extract tank signals
-[MFFTi, Sffti, fstep] = f_DF_electric(tank_temp, Fs, f_up, f_down);
+[MFFTi, Sffti, fstep] = f_DF_electric(tank_temp, fs, f_up, f_down);
 
 for i = 1:2:60  % Grouping 2 electrodes per plot
     figure();
@@ -175,8 +182,8 @@ t_tank = 1/fs*(0:L_tank-1);
 
 in_time = 1;
 end_time = 3;
-in_sample = in_time * Fs + 1;
-end_sample = end_time * Fs;
+in_sample = in_time * fs + 1;
+end_sample = end_time * fs;
 time = linspace(in_time, end_time, length(in_sample:end_sample));
 
 % Detrend MEA signals
@@ -198,18 +205,19 @@ for mea_idx = 1:3
     % Set the indices for each MEA group
     if mea_idx == 1
         mea_title = 'RIGHT ATRIUM';
-        electrode_idx = i + (mea_idx-1) * 16;
+        mea_range = 1:16;
     elseif mea_idx == 2
         mea_title = 'VENTRICLE';
-        electrode_idx = i + (mea_idx-1) * 16;
+        mea_range = 17:32;
     else
         mea_title = 'LEFT ATRIUM';
-        electrode_idx = i + 64;
+        mea_range = 65:80;
+        
     end
     % Loop through each set of 4 electrodes
     for i = 1:4:16
         figure(); % Create a new figure for each group of 4 electrodes
-        sgtitle(['MEA ' num2str(mea_idx) ' - ' mea_title]);
+        sgtitle(['MEA ' num2str(mea_idx) ' - ' mea_title], 'FontWeight', 'bold');
         
         % Loop through the 4 electrodes in the group
         for j = 0:3
@@ -225,7 +233,7 @@ for mea_idx = 1:3
         end
         
         % Save each figure
-        saveas(gcf, ['MEA_' num2str(mea_idx) '_Electrodes_' num2str(electrode_idx) '-' num2str(electrode_idx+3) '_detrend.png']);
+%         saveas(gcf, ['MEA_' num2str(mea_idx) '_Electrodes_' num2str(mea_range(i)) '-' num2str(mea_range(i+3)) '_detrend.png']);
       
     end
 end
@@ -256,7 +264,7 @@ for i = 1:4:60
     end
     
     % Save each figure
-%     saveas(gcf, ['Tank_Electrodes_' num2str(i) '-' num2str(i+3) '_detrend.png']);
+    saveas(gcf, ['Tank_Electrodes_' num2str(tank_electrodes(i)) '-' num2str(tank_electrodes(i+3)) '_detrend.png']);
 end
 
 %% Downsampling
@@ -276,6 +284,12 @@ fs_tank_down = fs / factor;  % Update Tank sampling frequency
 fs_down = fs / factor;
 
 %% Plot MEAs electrical signals before and after downsampling
+
+in_time = 1;
+end_time = 3;
+% Define time vectors for original and downsampled signals
+t_original = (in_time*fs+1:end_time*fs) / fs; % Time vector for the original signal
+t_down = (in_time*fs_down+1:end_time*fs_down) / fs_down; % Time vector for the downsampled signal
 
 for mea_idx = 1:3
     % Set the indices for each MEA group
@@ -306,16 +320,12 @@ for mea_idx = 1:3
         % Loop through the 2 electrodes in the group
         for j = 0:3
             electrode_idx = i + j;
-
-            % Define time vectors for original and downsampled signals (2 seconds)
-            t_original = (0:2*fs-1) / fs; % Time vector for the original signal
-            t_downsampled = (0:2*fs_down-1) / fs_down; % Time vector for the downsampled signal
             
             % Plot Original and Downsampled Electrical Signals for the current electrode
             subplot(2, 2, j + 1);
-            plot(t_original, meas_signal_d(mea_range(electrode_idx), 1:2*fs), 'r', 'LineWidth', 1);
+            plot(t_original, meas_signal_d(mea_range(electrode_idx), in_time*fs+1:end_time*fs), 'r', 'LineWidth', 1);
             hold on;
-            plot(t_downsampled, meas_signal_down(mea_range(electrode_idx), 1:2*fs_down), 'b', 'LineWidth', 1);
+            plot(t_down, meas_signal_down(mea_range(electrode_idx), in_time*fs_down+1:end_time*fs_down), 'b', 'LineWidth', 1);
             title(['Electrode ' num2str(plot_range(electrode_idx))]);
             xlabel('Time (s)');
             ylabel('Amplitude (\muV)');
@@ -324,12 +334,18 @@ for mea_idx = 1:3
         end
          
         % Save each figure (optional)
-        % saveas(gcf, ['MEA_' num2str(mea_idx) '_Electrodes_' num2str(i) '-' num2str(i+1) '_downsampling.png']);
+        saveas(gcf, ['MEA_' num2str(mea_idx) '_Electrodes_' num2str(plot_range(i)) '-' num2str(plot_range(i+1)) '_downsampling.png']);
     end
 end
 %% Plot Tank Signals before and after downsampling
 
 tank_electrodes = [129:174, 177:190];
+
+in_time = 1;
+end_time = 3;
+% Define time vectors for original and downsampled signals
+t_original = (in_time*fs+1:end_time*fs) / fs; % Time vector for the original signal
+t_down = (in_time*fs_down+1:end_time*fs_down) / fs_down; % Time vector for the downsampled signal
 
 for i = 1:4:60
     figure(); % Create a new figure for each group of 4 tank electrodes
@@ -340,15 +356,11 @@ for i = 1:4:60
         tank_idx = i + j;
         electrode_idx = tank_electrodes(tank_idx); % Map to the real electrode number
         
-        % Define time vectors for original and downsampled signals (2 seconds)
-        t_original = (0:2*fs-1) / fs; % Time vector for the original signal
-        t_downsampled = (0:2*fs_down-1) / fs_down; % Time vector for the downsampled signal
-        
         % Plot Original and Downsampled Electrical Signals for the current electrode
         subplot(2, 2, j + 1);
-        plot(t_original, tank_signal_d(tank_idx, 1:2*fs), 'r', 'LineWidth', 1);
+        plot(t_original, tank_signal_d(tank_idx, in_time*fs+1:end_time*fs), 'r', 'LineWidth', 1);
         hold on;
-        plot(t_downsampled, tank_signal_down(tank_idx, 1:2*fs_down), 'b', 'LineWidth', 1);
+        plot(t_down, tank_signal_down(tank_idx, in_time*fs_down+1:end_time*fs_down), 'b', 'LineWidth', 1);
         title(['Electrode ' num2str(electrode_idx)]);
         xlabel('Time (s)');
         ylabel('Amplitude (\muV)');
@@ -357,7 +369,7 @@ for i = 1:4:60
     end
     
     % Save each figure
-%   saveas(gcf, ['Tank_Electrodes_' num2str(tank_electrodes(i)) '-' num2str(tank_electrodes(i+3)) '_detrend.png']);
+  saveas(gcf, ['Tank_Electrodes_' num2str(tank_electrodes(i)) '-' num2str(tank_electrodes(i+3)) '_downsampling.png']);
 
 end
 
@@ -367,10 +379,10 @@ save('downsampled_signals.mat', 'meas_signal_down', 'tank_signal_down','fs_down'
 %% Normalizing signals
 
 % Normalizing meas signal
-meas_signal_norm = normalize(meas_signal_down,2,'zscore','robust');
+meas_signal_norm = normalize(meas_signal_down,2,'zscore','std');
 
 % Normalizing tank signals
-tank_signal_norm = normalize(tank_signal_down,2,'zscore','robust');
+tank_signal_norm = normalize(tank_signal_down,2,'zscore','std');
 
 %% Plotting MEAs signals before and after normalization
 
@@ -418,12 +430,12 @@ for mea_idx = 1:3
             title(['Electrode ' num2str(plot_range(electrode_idx))]);
             xlabel('Time (s)');
             ylabel('Amplitude (\muV)');
-            legend('Original', 'ROBUST Normalized');
+            legend('Original', 'Normalized');
 
         end
          
         % Save each figure
-        % saveas(gcf, ['MEA_' num2str(mea_idx) '_Electrodes_' num2str(plot_range(i)) '-' num2str(plot_range(i+3)) '_norm.png']);
+%         saveas(gcf, ['MEA_' num2str(mea_idx) '_Electrodes_' num2str(plot_range(i)) '-' num2str(plot_range(i+3)) '_norm.png']);
 
     end
 end
@@ -456,7 +468,7 @@ for i = 1:4:60
         title(['Electrode ' num2str(electrode_idx)]);
         xlabel('Time (s)');
         ylabel('Amplitude ($\mu$V)', 'Interpreter', 'latex');
-        legend('Original', 'ROBUST Normalized');
+        legend('Original', 'Normalized');
         
 
 %       Save each figure
@@ -464,3 +476,93 @@ for i = 1:4:60
 
     end
 end
+
+%% Scaling signals
+
+% Scaling meas signal
+meas_signal_scaled = rescale(meas_signal_norm, -1, 1);
+
+% Scaling tank signals
+tank_signal_scaled = rescale(tank_signal_norm, -1, 1);
+
+%% Plotting MEA signals before and after normalization and scaling
+
+in_time = 1;
+end_time = 3;
+in_sample = in_time * fs_down + 1;
+end_sample = end_time * fs_down;
+time = linspace(in_time, end_time, length(in_sample:end_sample));
+
+for mea_idx = 1:3
+    % Set the indices for each MEA group
+    if mea_idx == 1
+        mea_range = 1:16;
+        mea_title = 'RIGHT ATRIUM';  
+    elseif mea_idx == 2
+        mea_range = 17:32; 
+        mea_title = 'VENTRICLE'; 
+    else
+        mea_range = 33:48;
+        mea_title = 'LEFT ATRIUM'; 
+    end
+
+    if mea_idx == 3
+        plot_range = 65:80;
+    else
+        plot_range = mea_range;
+    end
+
+    % Loop through each set of 4 electrodes per plot
+    for i = 1:4:16
+        figure();
+        sgtitle(['MEA ' num2str(mea_idx) ' - ' mea_title], 'FontWeight', 'bold');
+
+        % Loop through the 4 electrodes in the group
+        for j = 0:3
+            electrode_idx = i + j;
+            
+            % Plot Original, Normalized, and Scaled Electrical Signals
+            subplot(2, 2, j + 1);
+            plot(time, meas_signal_norm(mea_range(electrode_idx), in_sample:end_sample), 'r', 'LineWidth', 1);
+            hold on;
+            plot(time, meas_signal_scaled(mea_range(electrode_idx), in_sample:end_sample), 'g', 'LineWidth', 1);
+            title(['Electrode ' num2str(plot_range(electrode_idx))]);
+            xlabel('Time (s)');
+            ylabel('Amplitude (\muV)');
+            legend('Original', 'Scaled');
+        end
+    end
+end
+
+
+%% Plotting Tank signals before and after normalization and scaling
+
+in_time = 0;
+end_time = 2;
+in_sample = in_time * fs_down + 1;
+end_sample = end_time * fs_down;
+time = linspace(in_time, end_time, length(in_sample:end_sample));
+
+tank_electrodes = [129:174, 177:190];
+
+for i = 1:4:60
+    figure(); % Create a new figure for each group of 4 tank electrodes
+    sgtitle('Tank Signals', 'FontWeight', 'bold');
+    
+    % Loop through the 4 tank electrodes
+    for j = 0:3
+        tank_idx = i + j;
+        electrode_idx = tank_electrodes(tank_idx); % Map to the real electrode number
+        
+        % Plot Original, Normalized, and Scaled Electrical Signals
+        subplot(2, 2, j + 1);
+        plot(time, tank_signal_norm(tank_idx, in_sample:end_sample), 'r', 'LineWidth', 1);
+        hold on;
+        plot(time, tank_signal_scaled(tank_idx, in_sample:end_sample), 'g', 'LineWidth', 1);
+        title(['Electrode ' num2str(electrode_idx)]);
+        xlabel('Time (s)');
+        ylabel('Amplitude ($\mu$V)', 'Interpreter', 'latex');
+        legend('Normalized', 'Scaled', 'Location', 'best');
+    end
+end
+
