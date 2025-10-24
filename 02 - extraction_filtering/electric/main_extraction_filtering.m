f3565e4df31d235ac08fd224cdbe15e09ff5c354
TainanNeves <tainancneves@gmail.com>
2025-10-24 12:49:40 -0300

    Update_HEARTLAB

--- 02 - extraction_filtering/electric/main_extraction_filtering.m
+++ 02 - extraction_filtering/electric/main_extraction_filtering.m
@@ -6,7 +6,7 @@ clear; clc;
 
 % fulfilename is the path to the structure.oebin file contained in one of
 % the experiments
-fullfilename = "F:\HEartLab\experiment_data\E28\Electric\1\No filter\2025-06-05_11-18-50\Record Node 108\experiment1\recording7\structure.oebin"; % Put the .oebin path
+fullfilename = "E:\experiment_data\E30\Electrical\02\noFilter\2025-07-15_12-45-13\Record Node 108\experiment1\recording3\structure.oebin"; % Put the .oebin path
 
 % Channels to save
 channels = [1:192];
@@ -67,12 +67,12 @@ time = linspace(ti, tf, (tf - ti) * Fs + 1);
 % Electrodes selection
 el1 = 22;  % RA
 el2 = 6; % LA
-el3 = 87; % V
-el4 = 142; % Tank
+el3 = 91; % V
+el4 = 144; % Tank
 Index = {'RA', 'LA', 'V','TANK'};
 % Filter range (Butterworth)
 f_low = 0.5;
-f_high = 250;
+f_high = 200;
 % Filter Configuration (Wavelet)
 waveletType = {'db4'}; % Wavelet type
 numLevels = 10; % Number of decomposition levels
